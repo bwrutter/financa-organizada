9c866049cbaef174ff2db073165e7c4897093493
bwrutter <bryanwrutter96@gmail.com>
2024-02-20 22:17:45 +0000

    chore: ajuste sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#TODO: Ajsutar para dar deploy localmente apos sls offline
+cd /workspace/scripts/ && ./deploy.sh &
 cd /workspace/client/app && npm run preview &
 cd /workspace/service/api && npm start
